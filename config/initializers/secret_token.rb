54ec74d99f7f425f2331f07b8336bc3693cfc0a2
treshone <treshone994s@gmail.com>
2019-07-16 15:33:46 +0500

    added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abaktask::Application.config.secret_key_base = '0bc96b4d6ff8e170c44ec6b5a52d47483dfaa89864cf0c17e6b12d547844e50ad8fa17cbb38c0cb369522615a7d7d26efc12c73482b0eca0ddfd3f057fc27256'
